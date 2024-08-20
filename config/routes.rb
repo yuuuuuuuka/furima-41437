fb7bd072184ec2d8669aab24bc44aa1bd03a17ab
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-20 14:31:59 +0900

    コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "items#index"
+  resources :items, only:[:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
