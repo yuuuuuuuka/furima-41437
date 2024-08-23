af04f3b76447d466e2e06f2d3ce7bac6d05868ba
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-23 15:22:47 +0900

    追記　修正です

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,5 @@ class User < ApplicationRecord
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
   validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
-  validates :password, length: { minimum: 6 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字で入力してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字で入力してください' }
 end
