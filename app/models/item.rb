49e0e90b59e1489dd4bd86327b6101b5f59b917a
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-24 21:58:07 +0900

    テストコード　実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,26 +4,20 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  validates :name, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :item_situation
-  validates :name, presence: true
   validates :item_situation_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :load
-  validates :name, presence: true
   validates :load_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :prefecture
-  validates :name, presence: true
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :shipping_location
-  validates :name, presence: true
   validates :shipping_location_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :name, presence: true
   validates :price, numericality: {
     greater_than_or_equal_to: 300,
     less_than_or_equal_to: 9_999_999,
